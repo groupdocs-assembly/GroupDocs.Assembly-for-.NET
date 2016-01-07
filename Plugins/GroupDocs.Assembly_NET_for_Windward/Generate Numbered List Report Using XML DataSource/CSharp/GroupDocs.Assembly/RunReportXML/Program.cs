168c4a880211e88b88d042ecad3758f11a53616a
Muhammad Atir <atirtahir@hotmail.com>
2016-01-07 19:01:22 +0500

    Numbered list plugin added

---
+++ Plugins/GroupDocs.Assembly_NET_for_Windward/Generate Numbered List Report Using XML DataSource/CSharp/GroupDocs.Assembly/RunReportXML/Program.cs
@@ -0,0 +1,52 @@
+﻿using GroupDocs.Assembly;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunReportXML
+{
+    //ExStart:Generate numbered list in assembly-Csharp
+    public class Program
+    {
+        public const string productXMLfile = "../../../../../Data/Data Source/Products.xml";
+        static void Main(string[] args)
+        {
+            //Setting up source open presentation template
+            FileStream template = File.OpenRead("../../../../../Data/Samples/Source/Numbered List_XML.docx");
+            //Setting up destination open presentation report 
+            FileStream output = File.Create("../../../../../Data/Samples/Destination/Xml Numbered List.docx");
+
+            try
+            {
+                //Instantiate DocumentAssembler class
+                DocumentAssembler assembler = new DocumentAssembler();
+                //Call AssembleDocument to generate Bulleted List Report in open presentation format
+                assembler.AssembleDocument(template, output, GetProductsData(), "product");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public static DataSet GetProductsData()
+        {
+            try
+            {
+                DataSet mainDs = new DataSet();
+                System.IO.FileStream fsReadXml = new System.IO.FileStream(productXMLfile, System.IO.FileMode.Open);
+                mainDs.ReadXml(fsReadXml, XmlReadMode.ReadSchema);
+                mainDs.Tables[0].TableName = "ds";
+                return mainDs;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+    //ExEnd:Generate numbered list in assembly-Csharp
+}
---
+++ Plugins/GroupDocs.Assembly_NET_for_Windward/Generate Numbered List Report Using XML DataSource/VisualBasic/GroupDocs.Assembly/RunReportXML/Module1.vb
@@ -0,0 +1,39 @@
+﻿Imports System.IO
+Imports GroupDocs.Assembly
+
+'ExStart:Generate CommonList in groupdocs.assembly-VB
+Module Module1
+
+    Public Const productXMLfile As String = "../../../../../Data/Data Source/Products.xml"
+    Sub Main()
+
+        'Setting up source open presentation template
+        Dim template As FileStream = File.OpenRead("../../../../../Data/Samples/Source/Numbered List_XML.docx")
+        'Setting up destination open presentation report 
+        Dim output As FileStream = File.Create("../../../../../Data/Samples/Destination/Xml Numbered List.docx")
+
+        Try
+            'Instantiate DocumentAssembler class
+            Dim assembler As New DocumentAssembler()
+            'Call AssembleDocument to generate single row Report in open presentation format
+            assembler.AssembleDocument(template, output, GetProductsData(), "product")
+        Catch ex As Exception
+            Console.WriteLine(ex.Message)
+        End Try
+
+
+    End Sub
+
+    Public Function GetProductsData() As DataSet
+        Try
+            Dim mainDs As New DataSet()
+            Dim fsReadXml As New System.IO.FileStream(productXMLfile, System.IO.FileMode.Open)
+            mainDs.ReadXml(fsReadXml, XmlReadMode.ReadSchema)
+            mainDs.Tables(0).TableName = "ds"
+            Return mainDs
+        Catch
+            Return Nothing
+        End Try
+    End Function
+End Module
+'ExEnd:Generate CommonList in groupdocs.assembly-VB
